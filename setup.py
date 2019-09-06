373356cd7f84ded17665bf814e357e5c1864734b
JeroenDM <jeroendemaeyer@live.be>
2019-09-06 16:20:14 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,31 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="acrobotics",
-    version="0.0.1",
+    version="0.0.2",
     author="Jeroen De Maeyer",
     author_email="jeroen.demaeyer@kuleuven.be",
+    description="Primitive robot kinematics and collision checking.",
     url="https://github.com/JeroenDM/acrobotics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(where="src"),
-    install_requires=["numpy", "matplotlib", "python-fcl", "tqdm"],
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "python-fcl",
+        "casadi",
+        "pyquaternion",
+        "tqdm",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
+    python_requires=">=3.6",
 )
