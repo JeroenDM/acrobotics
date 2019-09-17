f38abd5d008ee66396a7f5db84b6e19c23132500
JeroenDM <jeroendemaeyer@live.be>
2019-09-17 14:10:22 +0200

    add manifest and bump version

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name="acrobotics",
-    version="0.0.2",
+    version="0.0.3",
     author="Jeroen De Maeyer",
     author_email="jeroen.demaeyer@kuleuven.be",
     description="Primitive robot kinematics and collision checking.",
@@ -13,14 +16,8 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(where="src"),
-    install_requires=[
-        "numpy",
-        "matplotlib",
-        "python-fcl",
-        "casadi",
-        "pyquaternion",
-        "tqdm",
-    ],
+    install_requires=requirements,
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
